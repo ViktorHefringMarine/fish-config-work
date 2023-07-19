fe2cdc651cc65b3fb5d2eee820a6d20879902ff3
ViktorHefringMarine <viktor@hefringmarine.com>
2023-07-19 09:30:27 +0000

    ssh

--- abbrevations/ssh.fish
+++ abbrevations/ssh.fish
@@ -1,4 +1,4 @@
 
 # ssh
-abbr --add ssh-init 'eval $(ssh-agent -c) && ssh-add ~/.ssh/id_rsa'
-abbr --add ssh-set "eval \$(ssh-agent -c) && ssh-add ~/.ssh/work_pc_github"
+abbr --add ssh-init 'eval $(ssh-agent -c) && ssh-add ~/.ssh/github-viktorhefringmarine'
+# abbr --add ssh-set "eval \$(ssh-agent -c) && ssh-add ~/.ssh/"
